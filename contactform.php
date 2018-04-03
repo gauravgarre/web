f7de0929abce0c968095ab8ecaa793398548a165
Gaurav Garre <32738847+gauravgarre@users.noreply.github.com>
2018-04-03 08:37:22 -0400

    Centered webpage

--- contactform.php
+++ contactform.php
@@ -62,12 +62,14 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);  
+@mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
 <!-- include your own success html here -->
  
 Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
  
 }
 ?>
